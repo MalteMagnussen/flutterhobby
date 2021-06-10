226883c55458a37152a2919e78c29e2fe23289c6
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2021-06-10 20:46:44 +0200

    Added TextField and Popup for result

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'agify.dart';
+import 'agify_fetch.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,24 +27,70 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  late TextEditingController _controller;
+  late PersonsAge person;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Malte Hviid-Magnussen'),
       ),
-      body: const Center(
-        child: Text(
-          "Type in your name below, and I'll tell you what country you're from, and how old you are.",
-          textAlign: TextAlign.center,
-        ),
+      body: Column(
+        children: [
+          Center(
+            child: Text(
+              "Type in your name below, and I'll tell you what country you're from, and how old you are.",
+              textAlign: TextAlign.center,
+            ),
+          ),
+          TextField(
+            controller: _controller,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Input your Name',
+            ),
+            onSubmitted: (String value) async {
+              setState(() async {
+                person = await fetchPerson(value);
+              });
+              await showDialog<void>(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: Text('Your name is $value!'),
+                    content: Text(
+                      "Your age is ${person.age} and you're from ${person.country}!",
+                    ),
+                    actions: <Widget>[
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: const Text('OK'),
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
+          )
+        ],
       ),
 
-      floatingActionButton: const FloatingActionButton(
-        onPressed: null,
-        tooltip: 'Search',
-        child: Icon(Icons.search),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
