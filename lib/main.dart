a58b4796d53c63f34cf8dda776c038dc88dfbedc
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2021-06-10 22:19:03 +0200

    Added a button for submitting instead of enter on the keyboard, which didnt work on phones.

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,8 @@ class _MyHomePageState extends State<MyHomePage> {
   late TextEditingController _controller;
   late PersonsAge person;
   bool loading = false;
-
+  final _formKey = GlobalKey<FormState>();
+  late String value = "";
   @override
   void initState() {
     super.initState();
@@ -59,80 +60,105 @@ class _MyHomePageState extends State<MyHomePage> {
               style: TextStyle(height: 2),
             ),
           ),
-          SizedBox(
-            height: 50,
+          const SizedBox(
+            height: 30,
           ),
           loading
               ? const CircularProgressIndicator()
-              : SizedBox(
-                  width: 300,
-                  child: TextField(
-                    controller: _controller,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Input your Name',
-                    ),
-                    onSubmitted: (String value) async {
-                      value = value.trim();
-                      setState(() {
-                        loading = true;
-                      });
-                      try {
-                        if (value.length < 2) {
-                          throw Exception(
-                              "Name has to be longer than 2 characters.");
-                        }
-                        PersonsAge tempPerson = await fetchPerson(value);
-                        setState(() {
-                          person = tempPerson;
-                          loading = false;
-                        });
-                        await showDialog<void>(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return AlertDialog(
-                              title: Text('Your name is $value!'),
-                              content: Text(
-                                "Your age is ${person.age} and you're from ${person.country}!",
-                              ),
-                              actions: <Widget>[
-                                TextButton(
-                                  onPressed: () {
-                                    Navigator.pop(context);
-                                  },
-                                  child: const Text('OK'),
-                                ),
-                              ],
+              : Form(
+                  key: _formKey,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        width: 300,
+                        child: TextField(
+                          controller: _controller,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Input your Name',
+                          ),
+                          onChanged: (_value) => setState(() {
+                            value = _value;
+                          }),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      MaterialButton(
+                        height: 50,
+                        minWidth: 70,
+                        color: Theme.of(context).colorScheme.onPrimary,
+                        onPressed: () async {
+                          value = value.trim();
+                          if (value.length < 2) {
+                            return;
+                          }
+                          setState(() {
+                            loading = true;
+                          });
+                          try {
+                            if (value.length < 2) {
+                              throw Exception(
+                                  "Name has to be longer than 2 characters.");
+                            }
+                            PersonsAge tempPerson = await fetchPerson(value);
+                            setState(() {
+                              person = tempPerson;
+                              loading = false;
+                            });
+                            await showDialog<void>(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: Text('Your name is $value!'),
+                                  content: Text(
+                                    "Your age is ${person.age} and you're from ${person.country}!",
+                                  ),
+                                  actions: <Widget>[
+                                    TextButton(
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
+                                      child: const Text('OK'),
+                                    ),
+                                  ],
+                                );
+                              },
                             );
-                          },
-                        );
-                      } on Exception {
-                        await showDialog<void>(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return AlertDialog(
-                              title: const Text('Wow, your name is unique!'),
-                              content: const Text(
-                                "No one else is named that in the database.",
-                              ),
-                              actions: <Widget>[
-                                TextButton(
-                                  onPressed: () {
-                                    Navigator.pop(context);
-                                    setState(() {
-                                      loading = false;
-                                    });
-                                  },
-                                  child: const Text('OK'),
-                                ),
-                              ],
+                          } on Exception {
+                            await showDialog<void>(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title:
+                                      const Text('Wow, your name is unique!'),
+                                  content: const Text(
+                                    "No one else is named that in the database.",
+                                  ),
+                                  actions: <Widget>[
+                                    TextButton(
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                        setState(() {
+                                          loading = false;
+                                        });
+                                      },
+                                      child: const Text('OK'),
+                                    ),
+                                  ],
+                                );
+                              },
                             );
-                          },
-                        );
-                      }
-                    },
+                          }
+                        },
+                        child: const Text("Go!",
+                            style: TextStyle(color: Colors.black)),
+                      ),
+                    ],
                   ),
-                )
+                ),
         ],
       ),
     );
