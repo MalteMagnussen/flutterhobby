dca741a8ad263725a66cb73d34e5dd1a44c2af87
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2021-06-23 17:31:09 +0200

    Update museum_main.dart

---
+++ lib/MuseumFeature/museum_main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutterhobby/widget_view.dart';
+
+class MuseumWidget extends StatefulWidget {
+  @override
+  _MuseumWidgetController createState() => _MuseumWidgetController();
+}
+
+class _MuseumWidgetController extends State<MuseumWidget> {
+  @override
+  Widget build(BuildContext context) => _MuseumWidgetView(this);
+}
+
+class _MuseumWidgetView
+    extends WidgetView<MuseumWidget, _MuseumWidgetController> {
+  _MuseumWidgetView(_MuseumWidgetController state) : super(state);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
