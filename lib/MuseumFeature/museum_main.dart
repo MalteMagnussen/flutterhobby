beae23491a8290ddf328b21a5b8bddef905ff0e3
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2021-06-25 17:05:47 +0200

    html web renderer

--- lib/MuseumFeature/museum_main.dart
+++ lib/MuseumFeature/museum_main.dart
@@ -1,7 +1,6 @@
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutterhobby/widget_view.dart';
 
 import '../drawer.dart';
@@ -22,20 +21,10 @@ class _MuseumWidgetController extends State<MuseumWidget> {
     super.initState();
   }
 
-  Future<Uint8List> getArt(int id) async {
+  Future<String> getArt2(int id) async {
     Artwork _tempArt = await fetchArtwork(id);
     print(_tempArt.primaryImage);
-    ByteData imageData = await NetworkAssetBundle(
-      Uri.parse(_tempArt.primaryImage),
-    ).load("");
-    print(imageData);
-    Uint8List _bytes = imageData.buffer.asUint8List();
-    print(_bytes);
-    setState(() {
-      art = _tempArt;
-      bytes = _bytes;
-    });
-    return _bytes;
+    return _tempArt.primaryImage;
   }
 
   @override
@@ -54,10 +43,12 @@ class _MuseumWidgetView
         title: const Text("Explore Art"),
       ),
       body: Center(
-        child: FutureBuilder<Uint8List>(
-          future: state.getArt(49257),
+        child: FutureBuilder<String>(
+          future: state.getArt2(49257),
           builder: (context, snapshot) {
+            print(snapshot.connectionState);
             switch (snapshot.connectionState) {
+              case ConnectionState.active:
               case ConnectionState.waiting:
                 return const CircularProgressIndicator();
               case ConnectionState.done:
@@ -65,7 +56,7 @@ class _MuseumWidgetView
                   return Text('Error ${snapshot.error}');
                 }
                 if (snapshot.hasData) {
-                  return Image.memory(state.bytes);
+                  return Image.network(snapshot.data!);
                 }
             }
             return Text("Something went wrong.");
