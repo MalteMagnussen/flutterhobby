7621da522f21a352d28613f2e7566c93df56a0e7
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2021-12-11 15:01:48 +0100

    asd

--- lib/AstronomyPhotos/apod_fetch.dart
+++ lib/AstronomyPhotos/apod_fetch.dart
@@ -5,7 +5,7 @@ import 'apod_model.dart';
 // https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2017-07-08&hd=true
 
 Future<Apod> fetchAPOD(DateTime date) async {
-  const String nasaApiKey = "DEMO_KEY";
+  const String nasaApiKey = 'DEMO_KEY';
   final response = await http.get(
     Uri.parse(
       'https://api.nasa.gov/planetary/apod?api_key=$nasaApiKey&date=${date.year}-${date.month}-${date.day}&hd=true',
